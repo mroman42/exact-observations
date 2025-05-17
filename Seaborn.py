f7de2923ff28c5831cbd3acf1ebf8b9709925468
mroman42 <mromang08@gmail.com>
2025-05-17 16:28:57 +0100

    clean repository

--- Seaborn.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-heatmap_data = np.loadtxt("tempfile.txt", delimiter=",")
-sns.heatmap(heatmap_data, cmap="mako")
-plt.show()
